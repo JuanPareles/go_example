99c8a875fbd4352acef0c7a24c73485196767709
Juan Pareles <juanpareles@gmail.com>
2019-05-19 00:58:19 -0400

    default message + test

--- hello.go
+++ hello.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Aloha mondo"
+const englishHelloPrefix = "Hello, "
+
+// Hello : return a hello "something"
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
